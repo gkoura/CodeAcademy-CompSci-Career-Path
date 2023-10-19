17b278da0f0e32cf2ae1efc0c1e8c60703e034bd
Grei231 <gkouras@icloud.com>
2023-10-20 02:48:31 +0300

    updated formats on physics

--- 8. physics.py
+++ 8. physics.py
@@ -7,19 +7,19 @@ def f_to_c (f_temp):
     c_temp = (f_temp-32)*5/9
     return c_temp
 f100_in_celsius = f_to_c (100)
-print (f100_in_celsius)
+print (f"{f100_in_celsius:.2f}")
 
 
 def c_to_f (c_temp):
     f_temp = c_temp*9/5 + 32
     return f_temp
 c0_in_fahrenheit  = c_to_f (0)
-print (c0_in_fahrenheit)
+print (f"{c0_in_fahrenheit:.2f}")
 
 
 def get_force (mass, accelaration):
     return (mass*accelaration)
-print (f"The GE train supplies {get_force(train_mass,train_acceleration)} Newtons of force.")
+print (f"The GE train supplies {get_force(train_mass,train_acceleration):.2e} Newtons of force.")
 
 def get_energy (mass, c=3*10**8):
     return mass*(c**2)
